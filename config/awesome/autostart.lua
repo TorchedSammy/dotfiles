d00903f935c57a153024d9073035437a542aebd7
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-02-18 22:30:35 -0400

    use lua script for autostart

--- config/awesome/autostart.lua
+++ config/awesome/autostart.lua
@@ -1,4 +1,5 @@
 local awful = require('awful')
+local beautiful = require('beautiful')
 
 awful.spawn('xmodmap -e "pointer = 3 2 1"')
-awful.spawn('picom -b')
+awful.spawn('picom -b --config '..os.getenv('HOME').."/.config/picom/picom"..(themename and '-'..themename or '')..'.conf')
