b1367faafb9b4228bf949fd80f6e81e1bf658385
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-02-23 11:42:05 -0400

    remove some files

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -212,7 +212,6 @@ client.connect_signal("manage", function (c)
 		-- Prevent clients from being unreachable after screen count changes.
 		awful.placement.no_offscreen(c)
 	end
-	--c.shape = gears.shape.rounded_rect
 end)
 
 require('titlebars/'..(beautiful.titlebar_type and beautiful.titlebar_type or 'default'))
