dff3afe31e3e906fcefee7a6f38366bd1bc4bf95
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-02-23 12:06:13 -0400

    move layout images to 1 single folder

--- config/awesome/themes/default/theme.lua
+++ config/awesome/themes/default/theme.lua
@@ -5,8 +5,12 @@ local xrdb = xresources.get_current_theme()
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
+local config_path = gfs.get_configuration_dir()
 local helpers = require("helpers")
 
+function layoutimg(name)
+	return config_path..'images/layouts/'..name
+end
 local theme = {}
 
 -- Get colors from .Xresources and set fallback colors
@@ -155,22 +159,22 @@ theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/m
 theme.wallpaper = gfs.get_configuration_dir().."/wallpapers/primary.jpg"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairh = layoutimg("fairhw.png")
+theme.layout_fairv = layoutimg("fairvw.png")
+theme.layout_floating  = layoutimg("floatingw.png")
+theme.layout_magnifier = layoutimg("magnifierw.png")
+theme.layout_max = layoutimg("maxw.png")
+theme.layout_fullscreen = layoutimg("fullscreenw.png")
+theme.layout_tilebottom = layoutimg("tilebottomw.png")
+theme.layout_tileleft   = layoutimg("tileleftw.png")
+theme.layout_tile = layoutimg("tilew.png")
+theme.layout_tiletop = layoutimg("tiletopw.png")
+theme.layout_spiral  = layoutimg("spiralw.png")
+theme.layout_dwindle = layoutimg("dwindlew.png")
+theme.layout_cornernw = layoutimg("cornernww.png")
+theme.layout_cornerne = layoutimg("cornernew.png")
+theme.layout_cornersw = layoutimg("cornersww.png")
+theme.layout_cornerse = layoutimg("cornersew.png")
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
