b6d578715e0e4ab38b92db75bffb12c154326df7
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-02-27 12:05:52 -0400

    remove debian things

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -20,7 +20,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
 -- Load Debian menu entries
-local debian = require("debian.menu")
 local has_fdo, freedesktop = pcall(require, "freedesktop")
 
 -- {{{ Error handling
@@ -112,7 +111,6 @@ else
 	mymainmenu = awful.menu({
 		items = {
 				  menu_awesome,
-				  { "Debian", debian.menu.Debian_menu.Debian },
 				  menu_terminal,
 				}
 	})
