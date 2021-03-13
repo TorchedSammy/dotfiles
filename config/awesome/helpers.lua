f66a1126ba32ecc4a170a1c98b6cfa997bdeccf4
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-12 22:42:55 -0400

    move set_wallpapers function to helpers

--- config/awesome/helpers.lua
+++ config/awesome/helpers.lua
@@ -14,4 +14,14 @@ helpers.colorize_text = function(text, color)
     return "<span foreground='" .. color .."'>" .. text .. "</span>"
 end
 
+helpers.set_wallpaper = function (s)
+	if beautiful.wallpaper then
+		local wallpaper = beautiful.wallpaper
+		if type(wallpaper) == "function" then
+			wallpaper = wallpaper(s)
+		end
+		gears.wallpaper.maximized(wallpaper, s, true)
+	end
+end
+
 return helpers
