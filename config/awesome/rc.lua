99fb5f7090a18de8b896cb1065b2268fb75688c8
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-13 22:29:19 -0400

    change back theme to clouds

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -2,7 +2,7 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
-themename = 'macos'
+themename = 'clouds'
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
