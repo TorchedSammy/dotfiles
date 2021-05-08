e4be99d5ab711e9c09931b002faa2eb8c36f5347
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-05-08 12:45:21 -0400

    refactor: remove all configs temporarily

--- .hilbishrc.lua
+++
@@ -1,9 +0,0 @@
--- Default Hilbish config
-ansikit = require 'ansikit'
-
-os.setenv('PATH', os.getenv 'PATH' .. ':' .. os.getenv 'HOME' .. '/bin:/usr/local/go/bin')
-prompt(ansikit.text(
-	'{blue}%u {cyan}%d {green}∆{reset} '
-))
-
---hook("tab complete", function ())
--- config/awesome/autostart.lua
+++
@@ -1,6 +0,0 @@
-local awful = require('awful')
-local beautiful = require('beautiful')
-local naughty = require('naughty')
-
-awful.spawn('xmodmap -e "pointer = 3 2 1"')
-awful.spawn('picom --config '..os.getenv('HOME')..'/.config/picom/'..(beautiful.picom_conf)..'.conf '..(beautiful.exp_picom_bknd and '--experimental-backends ' or '')..'-b')
