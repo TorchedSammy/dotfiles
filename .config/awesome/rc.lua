f37364315bf941508af139ce094dcd2c1165ffe7
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-05-09 21:28:23 -0400

    awesome: switch to new dracula inspired theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -2,7 +2,7 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, 'luarocks.loader')
 
-themename = 'clouds'
+themename = 'dracula'
 
 local awful = require 'awful'
 require 'awful.autofocus'
