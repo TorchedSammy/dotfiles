8c6d777668a6e81eb889f526346c4e2a4931fe93
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-05-15 19:58:48 -0400

    feat(awesome): add cover art for music notif

--- .config/awesome/ui/notifs/playerctl.lua
+++ .config/awesome/ui/notifs/playerctl.lua
@@ -1,13 +1,22 @@
+local gears = require 'gears'
 local naughty = require 'naughty'
 local widgets = require 'ui.widgets'
 
 awesome.connect_signal('bling::playerctl::title_artist_album',
-function (title, artist, art_path)
+function (title, artist, art)
 	naughty.notify {
 		title = title,
 		text = artist,
-		image = art_path
+		icon = gears.surface.load_uncached_silently(art)
 	}
 	widgets.music:set_markup_silently((artist and artist .. ' - ' or '') .. title)
+end)
+
+awesome.connect_signal('bling::playerctl::no_players', function()
+	naughty.notify {
+		title = 'Party\'s Over!',
+		text = 'No more music playing.'
+	}
+	widgets.music:set_markup_silently 'Nothing Playing'
 end)
 
