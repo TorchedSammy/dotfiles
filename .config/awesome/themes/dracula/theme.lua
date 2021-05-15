417724e932025b3161649000b836c5db12d5aab4
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-05-15 12:29:15 -0400

    fix(awesome): use colors that match theme

--- .config/awesome/themes/dracula/theme.lua
+++ .config/awesome/themes/dracula/theme.lua
@@ -1,26 +1,26 @@
-local xresources = require('beautiful.xresources')
+local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local theme = require('themes.common')
 
-theme.xbackground = '#110f1c'
-theme.xforeground = '#ebe1e7'
-theme.xcolor0     = '#110f1c'
-theme.xcolor1     = '#72BCE0'
-theme.xcolor2     = '#AB93A0'
-theme.xcolor3     = '#D2A1B0'
-theme.xcolor4     = '#E4B5CC'
-theme.xcolor5     = '#A4C8DE'
-theme.xcolor6     = '#EDCCD6'
-theme.xcolor7     = '#ebe1e7'
-theme.xcolor8     = '#a49da1'
-theme.xcolor9     = '#72BCE0'
-theme.xcolor10    = '#AB93A0'
-theme.xcolor11    = '#D2A1B0'
-theme.xcolor12    = '#E4B5CC'
-theme.xcolor13    = '#A4C8DE'
-theme.xcolor14    = '#EDCCD6'
-theme.xcolor15    = '#ebe1e7'
+theme.xbackground = '#090b14'
+theme.xforeground = '#f5edfa'
+theme.xcolor0     = '#090b14'
+theme.xcolor1     = '#f04a5d'
+theme.xcolor2     = '#5ae696'
+theme.xcolor3     = '#fed06e'
+theme.xcolor4     = '#0badff'
+theme.xcolor5     = '#ff5ca0'
+theme.xcolor6     = '#00eaff'
+theme.xcolor7     = '#f5edfa'
+theme.xcolor8     = '#090b14'
+theme.xcolor9     = '#f04a5d'
+theme.xcolor10    = '#5ae696'
+theme.xcolor11    = '#fed06e'
+theme.xcolor12    = '#0badff'
+theme.xcolor13    = '#ff5ca0'
+theme.xcolor14    = '#00eaff'
+theme.xcolor15    = '#f5edfa'
 
 theme.bg_normal     = theme.xbackground
 theme.bg_sec        = '#6F6F70'
@@ -30,16 +30,37 @@ theme.bg_minimize   = theme.bg_normal
 
 theme.fg_normal     = theme.xforeground
 
+theme.useless_gap   = dpi(10)
+theme.border_width  = dpi(6)
+theme.border_normal  = theme.bg_normal -- Outer Border
+theme.border_focus  = theme.bg_normal -- Outer Border
+
 theme.titlebars = true
 theme.titlebar_type = 'default'
-theme.titlebar_bg = '#332b3d' .. 'e6'
+theme.titlebar_bg = theme.xcolor8
 
---theme.bar = 'bar here'
-theme.wibar_bg = theme.bg_normal .. 'd6'
+theme.bar = 'default'
+theme.wibar_bg = theme.bg_normal
+theme.wibar_spacing = dpi(14)
 
-theme.wallpaper = theme.config_path .. '/wallpapers/woman-sitting-at-pc.jpg'
+theme.notification_border_color = theme.titlebar_bg
+theme.notification_border_width = theme.border_width
+theme.notification_position = 'bottom_right'
+theme.notification_padding = dpi(45)
+theme.notification_spacing = dpi(8)
+theme.notification_shape = nil
 
-theme.picom_conf = 'blur-shadow'
+theme.taglist_text_font = 'Typicons 14'
+theme.taglist_text_color_empty    = { theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7, theme.xcolor7 }
+theme.taglist_text_color_occupied  = { theme.xcolor1, theme.xcolor2, theme.xcolor3, theme.xcolor4, theme.xcolor5, theme.xcolor6, theme.xcolor1, theme.xcolor2, theme.xcolor3, theme.xcolor4 }
+theme.taglist_text_color_focused  = { theme.xcolor1, theme.xcolor2, theme.xcolor3, theme.xcolor4, theme.xcolor5, theme.xcolor6, theme.xcolor1, theme.xcolor2, theme.xcolor3, theme.xcolor4 }
+theme.taglist_text_color_urgent   = { theme.xcolor9, theme.xcolor10, theme.xcolor11, theme.xcolor12, theme.xcolor13, theme.xcolor14, theme.xcolor9, theme.xcolor10, theme.xcolor11, theme.xcolor12 }
+
+theme.bg_systray = theme.wibar_bg
+
+theme.wallpaper = theme.config_path.."/wallpapers/woman-sitting-at-pc.jpg"
+
+theme.picom_conf = "blur-shadow"
 theme.exp_picom_bknd = true
 
 return theme
