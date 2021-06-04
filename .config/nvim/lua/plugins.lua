6e6aae0ec53275481fb581f4a8b4af320ed70f7b
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-04 19:51:35 -0400

    feat(neovim): add inital version of lua config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+return require('packer').startup(function()
+	-- Packer is stupid so i have to have it here too
+	use 'wbthomason/packer.nvim'
+
+	-- Buffer line
+	-- Might replace with nvim-bufferline
+	use {'romgrk/barbar.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+
+	use 'kyazdani42/nvim-tree.lua'
+
+	use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},
+		config = function()
+			require('gitsigns').setup()
+		end
+	}
+end)
