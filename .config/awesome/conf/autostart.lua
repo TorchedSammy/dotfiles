66804b586e56df1e2b2d3b22b521a2e75e0fd725
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-06 14:32:41 -0400

    awesome: make picom easy toggle

--- .config/awesome/conf/autostart.lua
+++ .config/awesome/conf/autostart.lua
@@ -4,7 +4,9 @@ local naughty = require 'naughty'
 
 awful.spawn 'xmodmap -e "pointer = 3 2 1"'
 -- Picom
-awful.spawn.with_shell('picom --config ~/.config/picom/' .. beautiful.picom_conf
-.. '.conf ' .. (beautiful.exp_picom_bknd and '--experimental-backends ' or '')
-.. '-b')
+if picom then 
+	awful.spawn.with_shell('picom --config ~/.config/picom/' .. beautiful.picom_conf
+	.. '.conf ' .. (beautiful.exp_picom_bknd and '--experimental-backends ' or '')
+	.. '-b')
+end
 awful.spawn('flavours apply ' .. (beautiful.flavours_name or themename))
