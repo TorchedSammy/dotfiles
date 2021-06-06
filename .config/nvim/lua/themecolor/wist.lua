b1419669884282a3bbfb86cd1130f8357b987ad6
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-05 21:06:51 -0400

    neovim: change wist colorscheme color declarations

--- .config/nvim/lua/themecolor/wist.lua
+++ .config/nvim/lua/themecolor/wist.lua
@@ -1,6 +1,20 @@
 return {
-	red = '#f04a5d',
-	green = '#5ae696',
-	blue = '#0badff'
-	grey = '#55516e'
+	bg = "090b14", -- Background
+	bgli = '0e111f', -- Lighter background
+	bgvli = '171b30', -- Much lighter background
+	fg = 'f5edfa', -- Foreground/Text
+	fgli = 'e3e3e3', -- Lighter foreground
+	gui1 = 'f04a5d', -- Red
+	gui2 = '5ae696', -- Green
+	gui3 = 'FED06E', -- Yellow
+	gui4 = '0badff', -- Blue
+	gui5 = 'ff5ca0', -- Magenta
+	gui6 = '00eaff', -- Cyan
+	gui8 = '55516e', -- Some kind of grey for comments, etc
+	gui9 = 'ff79c6',
+	gui10 = '44475a',
+	gui11 = '4d4f68',
+	gui12 = '62d6e8',
+	gui13 = 'e3e3e3',
+	gui14 = '50fa7b',
 }
