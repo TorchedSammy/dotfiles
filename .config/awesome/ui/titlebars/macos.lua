855a93bccefca883333e3ce05ec476f1a5ad6e9e
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-17 16:58:37 -0400

    awesome: refactor macos titlebar

--- .config/awesome/ui/titlebars/macos.lua
+++ .config/awesome/ui/titlebars/macos.lua
@@ -2,8 +2,53 @@
 local awful = require 'awful'
 local beautiful = require 'beautiful'
 local gears = require 'gears'
+local naughty = require 'naughty'
 local helpers = require 'helpers'
 local wibox = require 'wibox'
+
+local function titlebarbtn(c, color_focus, color_unfocus, shp)
+    local tb =
+        wibox.widget {
+        	{
+        		widget = wibox.widget.textbox
+        	},
+        forced_width = beautiful.dpi(16),
+        forced_height = beautiful.dpi(16),
+        bg = color_focus .. 80,
+        shape = shp,
+        widget = wibox.container.background
+    }
+
+    local function update()
+        if client.focus == c then
+            tb.bg = color_focus
+        else
+            tb.bg = color_unfocus
+        end
+    end
+    update()
+
+    c:connect_signal("focus", update)
+    c:connect_signal("unfocus", update)
+
+    tb:connect_signal(
+        "mouse::enter",
+        function()
+        	local clr = client.focus ~= c and color_focus or color_focus .. 55
+            tb.bg = clr
+        end
+    )
+    tb:connect_signal(
+        "mouse::leave",
+        function()
+        	local clr = client.focus == c and color_focus or color_unfocus
+            tb.bg = clr
+        end
+    )
+
+    tb.visible = true
+    return tb
+end
 
 client.connect_signal('request::titlebars', function(c)
 	-- buttons for the titlebar
@@ -17,42 +62,37 @@ client.connect_signal('request::titlebars', function(c)
 			awful.mouse.client.resize(c)
 		end)
 	)
-	function titlebarbtn(char, color, func)
-		return wibox.widget {
-			{
-				{
-					markup = helpers.colorize_text(char, color),
-					widget = wibox.widget.textbox,
-				},
-				top = 4, bottom = 4, 
-				widget = wibox.container.margin
-			},
-			bg = color,
-			widget = wibox.container.background,
-			buttons = gears.table.join(awful.button({}, 1, func)),
-			shape = gears.shape.circle
-		}
-	end
 
-	local minimize = titlebarbtn('', beautiful.xcolor3, function()
-		c.minimized = true
-		awful.client.next(1)
-        if client.focus then
-            client.focus:raise()
+	local close = titlebarbtn(c, beautiful.xcolor1, beautiful.xcolor8 .. 55, gears.shape.circle)
+    close:connect_signal(
+        "button::press",
+        function()
+            c:kill()
         end
-	end)
-	local maximize = titlebarbtn('', beautiful.xcolor2, function()
-        helpers.maximize(c)
-	end)
-	local close = titlebarbtn('', beautiful.xcolor1, function()
-		c:kill()
-	end)
+    )
+
+    local minimize = titlebarbtn(c, beautiful.xcolor3, beautiful.xcolor8 .. 55, gears.shape.circle)
+
+    minimize:connect_signal(
+        "button::press",
+        function()
+			c.minimized = true
+        end
+    )
+
+    local maximize = titlebarbtn(c, beautiful.xcolor2, beautiful.xcolor8 .. 55, gears.shape.circle)
+
+    maximize:connect_signal(
+        "button::press",
+        function()
+        	helpers.maximize(c)
+        end
+    )
 
 	awful.titlebar(c): setup {
 		layout = wibox.layout.align.horizontal,
 		expand = 'none',
 		{
-			buttons = buttons,
 			{
 				layout = wibox.layout.fixed.horizontal,
 				spacing = beautiful.wibar_spacing,
