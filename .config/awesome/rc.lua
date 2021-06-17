5abc969e261c9d72aad6dbff5ec6f5b5e7e7daf2
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-17 19:04:36 -0400

    awesome: add wip rooftop theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -2,7 +2,7 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, 'luarocks.loader')
 
-themename = 'hibis'
+themename = 'rooftop'
 picom = true
 
 local awful = require 'awful'
