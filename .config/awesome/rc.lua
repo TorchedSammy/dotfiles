269d04ceb72542f37d8e070e51908ff24a978100
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-17 08:18:18 -0400

    awesome: add hibis theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -2,7 +2,7 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, 'luarocks.loader')
 
-themename = 'clouds'
+themename = 'hibis'
 picom = true
 
 local awful = require 'awful'
