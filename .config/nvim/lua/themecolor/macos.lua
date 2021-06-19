92066cd194d07e25da6ab908961255f7af63be9e
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-19 09:16:06 -0400

    neovim: add macos colorscheme

---
+++ .config/nvim/lua/themecolor/macos.lua
@@ -0,0 +1,21 @@
+return {
+	bg = "0e1112", -- Background
+	bgli = '181d1f', -- Lighter background
+	bgvli = '212629', -- Much lighter background
+	fg = 'dadde0', -- Foreground/Text
+	fgli = 'ebeced', -- Lighter foreground
+	gui1 = 'f05959', -- Red
+	gui2 = '5ee26b', -- Green
+	gui3 = 'f3b94c', -- Yellow
+	gui4 = '307cf5', -- Blue
+	gui5 = 'f266c8', -- Magenta
+	gui6 = '6ef0e3', -- Cyan
+	gui8 = '666b70', -- Some kind of grey for comments, etc
+	gui9 = 'e25e9c',
+	gui10 = '35383b',
+	gui11 = '4e5256',
+	gui12 = '666b70',
+	gui13 = 'f0f2f5',
+	gui14 = '5ee26b',
+}
+
