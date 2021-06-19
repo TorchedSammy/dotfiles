3f97d009cc32ce83992e0a1777b237a163f651ad
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-18 20:35:19 -0400

    awesome: refactor macos theme
    did a bunch of stuff
    made the window class indicator per screen, so it wont change on both of
    my bars. i also made the workspace indicator per screen
    window class indicator is also nicer in general, class names are spaced
    out if they have - or _ in between and changed to title case, so
    `Sublime_text` -> `Sublime Text`
    google chrome is now just `Chrome` since thats what it is on macos?
    colors are closer to macos dark theme now (no light theme support, ever)
    titlebar is more macos-y, buttons on left and window title,
    unfocused clients have titlebar buttons darkened
    fixed wibar and made it transparent, make it a bit more minimal too
    and also made everything relating to macos theme up to date with current
    config

--- .config/awesome/ui/titlebars/macos.lua
+++ .config/awesome/ui/titlebars/macos.lua
@@ -7,47 +7,42 @@ local helpers = require 'helpers'
 local wibox = require 'wibox'
 
 local function titlebarbtn(c, color_focus, color_unfocus, shp)
-    local tb =
-        wibox.widget {
-        	{
-        		widget = wibox.widget.textbox
-        	},
-        forced_width = beautiful.dpi(16),
-        forced_height = beautiful.dpi(16),
-        bg = color_focus .. 80,
-        shape = shp,
-        widget = wibox.container.background
-    }
+	local ico = wibox.widget {
+		markup = '',
+		widget = wibox.widget.textbox
+	}
+	local tb = wibox.widget {
+		ico,
+		forced_width = beautiful.dpi(16),
+			forced_height = beautiful.dpi(16),
+			bg = color_focus .. 80,
+			shape = shp,
+			widget = wibox.container.background
+	}
 
-    local function update()
-        if client.focus == c then
-            tb.bg = color_focus
-        else
-            tb.bg = color_unfocus
-        end
-    end
-    update()
+	local function update()
+		if client.focus == c then
+				tb.bg = color_focus
+		else
+				tb.bg = color_unfocus
+		end
+	end
+	update()
 
-    c:connect_signal("focus", update)
-    c:connect_signal("unfocus", update)
+	c:connect_signal('focus', update)
+	c:connect_signal('unfocus', update)
 
-    tb:connect_signal(
-        "mouse::enter",
-        function()
-        	local clr = client.focus ~= c and color_focus or color_focus .. 55
-            tb.bg = clr
-        end
-    )
-    tb:connect_signal(
-        "mouse::leave",
-        function()
-        	local clr = client.focus == c and color_focus or color_unfocus
-            tb.bg = clr
-        end
-    )
+	tb:connect_signal('mouse::enter', function()
+		local clr = client.focus ~= c and color_focus or color_focus .. 55
+		tb.bg = clr
+	end)
+	tb:connect_signal('mouse::leave', function()
+		local clr = client.focus == c and color_focus or color_unfocus
+		tb.bg = clr
+	end)
 
-    tb.visible = true
-    return tb
+	tb.visible = true
+	return tb
 end
 
 client.connect_signal('request::titlebars', function(c)
@@ -64,64 +59,56 @@ client.connect_signal('request::titlebars', function(c)
 	)
 
 	local close = titlebarbtn(c, beautiful.xcolor1, beautiful.xcolor8 .. 55, gears.shape.circle)
-    close:connect_signal(
-        "button::press",
-        function()
-            c:kill()
-        end
-    )
+	close:connect_signal('button::press', function()
+		c:kill()
+	end)
 
-    local minimize = titlebarbtn(c, beautiful.xcolor3, beautiful.xcolor8 .. 55, gears.shape.circle)
+	local minimize = titlebarbtn(c, beautiful.xcolor2, beautiful.xcolor8 .. 55, gears.shape.circle)
+	minimize:connect_signal('button::press', function()
+		c.minimized = true
+	end)
 
-    minimize:connect_signal(
-        "button::press",
-        function()
-			c.minimized = true
-        end
-    )
+	local maximize = titlebarbtn(c, beautiful.xcolor3, beautiful.xcolor8 .. 55, gears.shape.circle)
+	maximize:connect_signal('button::press', function()
+		helpers.maximize(c)
+	end)
 
-    local maximize = titlebarbtn(c, beautiful.xcolor2, beautiful.xcolor8 .. 55, gears.shape.circle)
-
-    maximize:connect_signal(
-        "button::press",
-        function()
-        	helpers.maximize(c)
-        end
-    )
-
-	awful.titlebar(c): setup {
+	awful.titlebar(c, {size = beautiful.dpi(32)}): setup {
 		layout = wibox.layout.align.horizontal,
-		expand = 'none',
 		{
 			{
+				close,
+				maximize,
+				minimize,
 				layout = wibox.layout.fixed.horizontal,
-				spacing = beautiful.wibar_spacing,
+				spacing = beautiful.dpi(10),
 			},
-			left = beautiful.wibar_spacing,
-			right = beautiful.wibar_spacing,
+			left = beautiful.dpi(12),
 			widget = wibox.container.margin,
 		},
 		{
-			buttons = buttons,
 			{
-				layout = wibox.layout.fixed.horizontal,
+				{ -- Title
+					align = 'center',
+					widget = awful.titlebar.widget.titlewidget(c)
+				},
+				layout = wibox.layout.flex.horizontal,
 				spacing = beautiful.wibar_spacing
 			},
 			left = beautiful.wibar_spacing,
 			right = beautiful.wibar_spacing,
 			widget = wibox.container.margin,
+			buttons = buttons,
 		},
 		{
 			{
 				layout = wibox.layout.fixed.horizontal,
 				spacing = beautiful.wibar_spacing,
-				minimize,
-				maximize,
-				close
 			},
 			left = beautiful.wibar_spacing,
 			right = beautiful.wibar_spacing,
 			widget = wibox.container.margin,
+			buttons = buttons,
 		}
 	}
 end)
