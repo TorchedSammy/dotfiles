b3a9954dc054c651232dcedcfec4975129e6252c
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-19 11:02:01 -0400

    awesome: add fixed size to notifications and remove border, add window border

--- .config/awesome/themes/macos/theme.lua
+++ .config/awesome/themes/macos/theme.lua
@@ -36,14 +36,19 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.useless_gap   = dpi(6)
+theme.border_width  = dpi(1)
+theme.border_normal  = theme.bg_sec
+theme.border_focus  = theme.bg_sec
 
 theme.titlebars = true
 theme.titlebar_type = 'macos'
 theme.titlebar_bg = theme.bg_normal
 
 theme.notification_bg = theme.xcolor0
-theme.notification_border_color = theme.xcolor7
-theme.notification_border_width = dpi(1)
+theme.notification_width = dpi(340)
+theme.notification_height = dpi(100)
+theme.notification_border_color = theme.xcolor0
+theme.notification_border_width = dpi(0)
 
 theme.bar = 'macos'
 theme.wibar_height = dpi(30)
