aa6bb8cddcef9e4393998efc69f223da7e6e6a12
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-12-02 15:48:30 -0400

    tym: init config

---
+++ .config/tym/config.lua
@@ -0,0 +1,14 @@
+local tym = require 'tym'
+local thm = dofile(tym.get_theme_path())
+
+-- todo: watch config file and autoreload
+
+tym.set('font', 'Victor Mono Nerd Font Medium 12')
+tym.set_config {
+	title = 'Tym',
+	padding_horizontal = 20,
+	padding_vertical = 16,
+	cursor_shape = 'ibeam',
+	color_window_background = thm.color_background
+}
+
---
+++ .config/tym/theme.lua
@@ -0,0 +1,29 @@
+local bg = '#0e1112'
+local fg = '#dadde0'
+
+return {
+  color_background = bg,
+  color_foreground = fg,
+  color_bold = fg,
+  color_cursor = fg,
+  color_cursor_foreground = bg,
+  color_highlight = fg,
+  color_highlight_foreground = bg,
+  color_0  = bg,
+  color_1  = '#f05959',
+  color_2  = '#5ee26b',
+  color_3  = '#f3b94c',
+  color_4  = '#307cf5',
+  color_5  = '#f266c8',
+  color_6  = '#6ef0e3',
+  color_7  = fg,
+  color_8  = '#666b70',
+  color_9  = '#e25e9c',
+  color_10 = '#35383b',
+  color_11 = '#4e5256',
+  color_12 = '#666b70',
+  color_13 = '#f0f2f5',
+  color_14 = '#5ee26b',
+  color_15 = '#f7f8fa',
+}
+
