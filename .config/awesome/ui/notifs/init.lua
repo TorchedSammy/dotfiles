e5eaa1fe849eb6e94bd23dd7f4f82d2398c5aff1
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-12-17 00:10:53 -0400

    awesome: add configurable border width for notifs

--- .config/awesome/ui/notifs/init.lua
+++ .config/awesome/ui/notifs/init.lua
@@ -5,6 +5,7 @@ naughty.config.spacing = beautiful.notification_spacing
 naughty.config.padding = beautiful.notification_padding
 naughty.config.defaults.margin = beautiful.notification_margin
 naughty.config.defaults.position = beautiful.notification_position
+naughty.config.defaults.border_width = beautiful.notification_border_width
 
 require 'ui.notifs.playerctl'
 
