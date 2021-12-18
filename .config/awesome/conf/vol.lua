59879ae12edd925bd0c3763c4807f75176fcede6
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-12-18 00:04:25 -0400

    style(awesome): use single quotes and call without braces

--- .config/awesome/conf/vol.lua
+++ .config/awesome/conf/vol.lua
@@ -1,42 +1,36 @@
-local wibox = require("wibox")
-local awful = require("awful")
-local beautiful = require("beautiful")
-local helpers = require("helpers")
-local gears = require("gears")
-local naughty = require 'naughty'
+local awful = require 'awful'
 local step = 5
-
 local volume = {}
 
 volume.get_volume_state = function(cb)
-  awful.spawn.easy_async("pactl list sinks", function(stdout)
-    local mute = stdout:match("Mute:%s+(%a+)")
-    local volume = stdout:match("%s%sVolume:[%s%a-:%d/]+%s(%d+)%%")
+  awful.spawn.easy_async('pactl list sinks', function(stdout)
+    local mute = stdout:match('Mute:%s+(%a+)')
+    local volpercent = stdout:match('%s%sVolume:[%s%a-:%d/]+%s(%d+)%%')
 
-    if mute == "yes" then
+    if mute == 'yes' then
       mute = true
     else
       mute = false
     end
 
-    volume = tonumber(volume)
+    volpercent = tonumber(volpercent)
 
-    cb(volume, mute)
+    cb(volpercent, mute)
   end)
 end
 
 volume.up = function()
-    awesome.spawn("pactl set-sink-volume @DEFAULT_SINK@ +"..step.."%")
-    -- awesome.emit_signal("evil::volume", next_vol, mute)
+    awesome.spawn('pactl set-sink-volume @DEFAULT_SINK@ +'..step..'%')
+    -- awesome.emit_signal('evil::volume', next_vol, mute)
 end
 
 volume.down = function()
-    awesome.spawn("pactl set-sink-volume @DEFAULT_SINK@ -"..step.."%")
-    -- awesome.emit_signal("evil::volume", next_vol, mute)
+    awesome.spawn('pactl set-sink-volume @DEFAULT_SINK@ -'..step..'%')
+    -- awesome.emit_signal('evil::volume', next_vol, mute)
 end
 
 volume.mute = function()
-    awesome.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")
+    awesome.spawn('pactl set-sink-mute @DEFAULT_SINK@ toggle')
 end
 
 return volume
