1c185f413ac0aed26482e960aa7eda384481f561
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-02-02 13:11:07 -0400

    awesome: dont use titlebar if type is none

--- .config/awesome/ui/init.lua
+++ .config/awesome/ui/init.lua
@@ -1,8 +1,9 @@
 local beautiful = require 'beautiful'
 
 require('ui.bars.' .. (beautiful.bar and beautiful.bar or 'default'))
-require('ui.titlebars.'
-.. (beautiful.titlebar_type and beautiful.titlebar_type or 'default'))
+if beautiful.titlebar_type ~= 'none' then
+	require('ui.titlebars.' .. (beautiful.titlebar_type and beautiful.titlebar_type or 'default'))
+end
 require 'ui.notifs'
 require 'ui.menu'
 
