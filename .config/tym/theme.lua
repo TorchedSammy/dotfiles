2f97ae5a54ead8b03af0c31c41bdf907a890f96e
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-02-06 12:00:37 -0400

    tym: change terminal theme based on awesome theme

--- .config/tym/theme.lua
+++ .config/tym/theme.lua
@@ -1,29 +1,25 @@
-local bg = '#0e1112'
-local fg = '#dadde0'
+local f = io.popen 'awesome-theme'
+local themename = f:read('*a'):gsub('\n', '')
+f:close()
 
-return {
-  color_background = bg,
-  color_foreground = fg,
-  color_bold = fg,
-  color_cursor = fg,
-  color_cursor_foreground = bg,
-  color_highlight = fg,
-  color_highlight_foreground = bg,
-  color_0  = bg,
-  color_1  = '#f05959',
-  color_2  = '#5ee26b',
-  color_3  = '#f3b94c',
-  color_4  = '#307cf5',
-  color_5  = '#f266c8',
-  color_6  = '#6ef0e3',
-  color_7  = fg,
-  color_8  = '#666b70',
-  color_9  = '#e25e9c',
-  color_10 = '#35383b',
-  color_11 = '#4e5256',
-  color_12 = '#666b70',
-  color_13 = '#f0f2f5',
-  color_14 = '#5ee26b',
-  color_15 = '#f7f8fa',
+local awesomeDir = '~/.config/awesome'
+local themePath = awesomeDir .. '/themes/colors/' .. themename .. '.lua'
+local thm = dofile(themePath:gsub('~', os.getenv('HOME')))
+local bg = thm.xbackground
+local fg = thm.xforeground
+
+local theme = {
+	color_background = bg,
+	color_foreground = fg,
+	color_bold = fg,
+	color_cursor = fg,
+	color_cursor_foreground = bg,
+	color_highlight = fg,
+	color_highlight_foreground = bg,
 }
 
+for i = 0, 15 do
+	theme['color_' .. i] = thm['xcolor' .. i]
+end
+
+return theme
