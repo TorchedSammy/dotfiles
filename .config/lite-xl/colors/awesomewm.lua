a39d983dfcddb01403c423b16f9a6b2dea373f77
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-03-18 10:24:48 -0400

    lite: make colors nicer and better and put more appropriate colors for certain groups

--- .config/lite-xl/colors/awesomewm.lua
+++ .config/lite-xl/colors/awesomewm.lua
@@ -8,33 +8,41 @@ local thm = dofile(themePath:gsub('~', os.getenv('HOME')))
 local bg = thm.xbackground
 local fg = thm.xforeground
 
+if themename == 'clouds' then
+	local green = thm.xcolor2
+	local yellow = thm.xcolor3
+	thm.xcolor2 = yellow
+	thm.xcolor3 = green
+end
+
 for i = 0, 15 do
 	style['color' .. i] = {common.color(thm['xcolor' .. i])}
 end
+
 style.gitdiff_padding = 4
 
 style.background = { common.color(bg) }
 style.background2 = { common.color(bg) }
 style.background3 = style.color9
 style.text = { common.color(fg) }
-style.caret = { common.color "#61efce" }
-style.accent = { common.color "#ffd152" }
-style.dim = { common.color "#615d5f" }
+style.caret = style.color4
+style.accent = style.color3
+style.dim = style.color10
 style.divider = { common.color "#242223" }
 style.selection = style.color10
 style.line_number = style.color11
 style.line_number2 = { common.color(fg) }
 style.line_highlight = style.color8
-style.scrollbar = { common.color "#454344" }
-style.scrollbar2 = { common.color "#524F50" }
+style.scrollbar = style.color10
+style.scrollbar2 = style.color11
 
 style.syntax["normal"] = { common.color(fg) }
 style.syntax["symbol"] = { common.color(fg) }
 style.syntax["comment"] = style.color11
 style.syntax["keyword"] = { common.color(thm.xcolor5) }
-style.syntax["keyword2"] = { common.color(thm.xcolor5) }
-style.syntax["number"] = { common.color "#ffd152" }
-style.syntax["literal"] = { common.color "#ffd152" }
+style.syntax["keyword2"] = { common.color(thm.xcolor6) }
+style.syntax["number"] = style.color1
+style.syntax["literal"] = style.color2
 style.syntax["string"] = { common.color(thm.xcolor2) }
 style.syntax["operator"] = { common.color(thm.xcolor3) }
 style.syntax["function"] = { common.color(thm.xcolor4) }
@@ -47,3 +55,10 @@ style.gitdiff_width = 2
 style.gitstatus_addition = style.gitdiff_addition
 style.gitstatus_modification = style.gitdiff_modification
 style.gitstatus_deletion = style.gitdiff_deletion
+
+style.lint = {
+	info = style.color4,
+	hint = style.color7,
+	warning = style.color3,
+	error = style.color1
+}
