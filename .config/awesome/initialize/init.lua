a6a0c6bffd8e4c433f89f099d8c9ce8ee35fa9b2
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-07-29 17:49:00 -0400

    awesome: notify if error occurs in theme-specific init code

--- .config/awesome/initialize/init.lua
+++ .config/awesome/initialize/init.lua
@@ -1,3 +1,13 @@
+local naughty = require 'naughty'
 local settings = require 'conf.settings'
 
-pcall(require, 'initialize.' .. settings.theme)
+if package.searchpath('initialize.' .. settings.theme, package.path) then
+	local ok, err = pcall(require, 'initialize.' .. settings.theme)
+	if not ok then
+		naughty.notify {
+			preset = naughty.config.presets.critical,
+			title = 'Error running theme-specific init code!',
+			text = err
+		}
+	end
+end
