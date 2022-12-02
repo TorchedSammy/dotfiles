c33e24fe772ac0e068381a814c5afbe2dc33eae2
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-12-02 10:24:38 -0400

    lite-xl: download miq if not installed, reorder plugin declaration

--- .config/lite-xl/init.lua
+++ .config/lite-xl/init.lua
@@ -1,10 +1,44 @@
 local core = require 'core'
 local config = require 'core.config'
+local command = require 'core.command'
 local keymap = require 'core.keymap'
 local style = require 'core.style'
 local StatusView = require 'core.statusview'
 local DocView = require 'core.docview'
 local CommandView = require 'core.commandview'
+
+local ok = pcall(require, 'plugins.miq')
+if not ok then
+	core.log 'Installing Miq...'
+	local proc = process.start {'git', 'clone', 'https://github.com/TorchedSammy/Miq', USERDIR .. '/plugins/miq'}
+	if proc then
+		proc:wait(process.WAIT_INFINITE)
+		if proc:returncode() == 0 then
+			command.perform 'core:restart'
+		else
+			print(proc:read_stdout() or proc:read_stderr())
+			core.error 'Could not install Miq.'
+		end
+	end
+end
+
+config.plugins.miq.lpm_prefix = HOME .. '/Downloads/lite-xl-plugin-manager/'
+config.plugins.miq.debug = true
+config.plugins.miq.plugins = {
+	-- miq can manage itself
+	'~/Files/Projects/Miq',
+	'TorchedSammy/Feathertime',
+	{'TorchedSammy/Litepresence', run = 'go get && go build'},
+	'TorchedSammy/Lazulight.lxl',
+	{'TorchedSammy/lite-xl-gitdiff-highlight', name = 'gitdiff_highlight'},
+	'lite-xl/lite-xl-lsp',
+	'TorchedSammy/lite-xl-lspkind',
+	-- others
+	'anthonyaxenov/lite-xl-ignore-syntax',
+	'juliardi/lite-xl-treeview-menu-extender',
+	'liquidev/lintplus',
+}
+
 local fontconfig = require 'plugins.fontconfig'
 local lspconfig = require 'plugins.lsp.config'
 local lspkind = require 'plugins.lspkind'
@@ -45,7 +79,8 @@ style.syntax_fonts = {
 	comment = italicFont,
 	keyword2 = italicFont,
 	type_builtin = italicFont,
-	error = italicFont
+	error = italicFont,
+	function_builtin = italicFont
 }
 for _, font in pairs(style.syntax_fonts) do
 	font:set_tab_size(4)
@@ -65,22 +100,6 @@ config.lint.lens_style = 'solid'
 config.plugins.lsp.stop_unneeded_servers = false
 config.plugins.plugin_manager.lpm_binary_path = HOME .. '/Downloads/lite-xl-plugin-manager/lpm'
 config.plugins.scale.mode = 'ui'
-config.plugins.miq.lpm_prefix = HOME .. '/Downloads/lite-xl-plugin-manager/'
-config.plugins.miq.debug = true
-config.plugins.miq.plugins = {
-	-- let miq manage itself from my dev dir
-	'~/Files/Projects/Miq',
-	'TorchedSammy/Feathertime',
-	'TorchedSammy/Litepresence',
-	'TorchedSammy/Lazulight.lxl',
-	'TorchedSammy/lite-xl-gitdiff-highlight',
-	'lite-xl/lite-xl-lsp',
-	'TorchedSammy/lite-xl-lspkind',
-	-- others
-	'anthonyaxenov/lite-xl-ignore-syntax',
-	'juliardi/lite-xl-treeview-menu-extender',
-	'liquidev/lintplus'
-}
 
 local bigCodeFont = style.code_font:copy((16 * 1.6) * SCALE)
 if not core.status_view:get_item 'icon:heart' then
