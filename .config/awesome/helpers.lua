76260913bb8ad559ab2ba19fde1555f0cedfd6f2
sammyette <torchedsammy@gmail.com>
2022-12-15 21:46:30 -0400

    awesome: fix color functions for dark theme, fix maximize margin

--- .config/awesome/helpers.lua
+++ .config/awesome/helpers.lua
@@ -1,7 +1,7 @@
 local awful = require 'awful'
-local gears = require 'gears'
-local wibox = require 'wibox'
 local beautiful = require 'beautiful'
+local gears = require 'gears'
+local settings = require 'conf.settings'
 
 local helpers = {}
 
@@ -37,7 +37,7 @@ function helpers.winmaxer(c)
   awful.placement.maximize(c, {
     honor_padding = true,
     honor_workarea = true,
-    margins = beautiful.useless_gap * 2
+    margins = beautiful.useless_gap * beautiful.dpi(2)
   })
 end
 
@@ -66,23 +66,35 @@ function helpers.shiftColor(color, percent)
   local g = (math.floor(num / 0x100) % 0x100) + percent
   local b = (num % 0x100) + percent
 
-  return string.format("%#x", clamp(r) * 0x10000 + clamp(g) * 0x100 + clamp(b)):gsub('0x', '#')
+  return string.format('%#x', clamp(r) * 0x10000 + clamp(g) * 0x100 + clamp(b)):gsub('0x', '#')
+end
+
+function helpers.invertColor(color)
+  local num = tonumber(color:sub(2), 16)
+  local r = 0xff - math.floor(num / 0x10000)
+  local g = 0xff - (math.floor(num / 0x100) % 0x100)
+  local b = 0xff - (num % 0x100)
+
+  return string.format('%#x', clamp(r) * 0x10000 + clamp(g) * 0x100 + clamp(b)):gsub('0x', '#')
 end
 
 function helpers.displayClickable(w, opts)
   opts = type(opts) == 'table' and opts or {}
+  opts.shiftFactor = opts.shiftFactor or -15
+  if settings.theme:match '-dark$' then opts.shiftFactor = opts.shiftFactor * -1 end
+
   local bgWid = w:get_children_by_id 'bg'[1]
   helpers.hoverCursor(w, opts.cursorType)
 
   w:connect_signal('mouse::enter', function()
     if bgWid then
-      bgWid.bg = opts.hoverColor and opts.hoverColor or helpers.shiftColor(opts.color, opts.shiftFacor and opts.shiftFactor or -15)
+      bgWid.bg = opts.hoverColor and opts.hoverColor or helpers.shiftColor(opts.color, opts.shiftFactor)
     end
   end)
 
   w:connect_signal('mouse::leave', function()
     if bgWid then
-      bgWid.bg = oldBg
+      bgWid.bg = opts.color
     end
   end)
 
