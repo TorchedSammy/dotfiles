8364cade3124f0ae4d2667ab6a9057d972d7f5e7
sammyette <torchedsammy@gmail.com>
2023-05-13 09:31:44 -0400

    awesome: improve autostart commands

--- .config/awesome/conf/autostart.lua
+++ .config/awesome/conf/autostart.lua
@@ -1,12 +1,27 @@
 local awful = require 'awful'
 local beautiful = require 'beautiful'
 local settings = require 'conf.settings'
+local naughty = require 'naughty'
 
---awful.spawn 'xmodmap -e "pointer = 3 2 1"'
 -- Picom
 if settings.picom then
 	awful.spawn.with_shell('picom --config ~/.config/picom/' .. beautiful.picom_conf
 	.. '.conf ' .. (beautiful.exp_picom_bknd and '--experimental-backends ' or '')
 	.. '-b')
 end
-awful.spawn('flavours apply ' .. (beautiful.flavours_name and beautiful.flavours_name or settings.theme))
+
+local programs = {
+	-- 'xmodmap -e "pointer = 3 2 1"',
+	'udiskie --tray',
+	'gcdemu',
+	'pactl load-module module-role-ducking trigger_roles=phone,event'
+}
+
+for _, p in ipairs(programs) do
+	awful.spawn.easy_async('pgrep ' .. p:match '^%w+', function(output)
+		if output == '' then
+			awful.spawn.easy_async(p, function() end)
+		end
+	end)
+end
+
