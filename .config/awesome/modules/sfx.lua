108bc2b05da36708c28814d15e176a074851ddf9
sammyette <torchedsammy@gmail.com>
2023-05-28 11:09:30 -0400

    awesome(modules/sfx): use easy async spawn function to remove loading cursor

--- .config/awesome/modules/sfx.lua
+++ .config/awesome/modules/sfx.lua
@@ -6,6 +6,11 @@ local sounds = {
 }
 
 local M = {}
+
+local function spawn(cmd)
+	awful.spawn.easy_async(cmd, function() end)
+end
+
 function M.play(s)
 	awful.spawn.easy_async(string.format('pacat --property=media.role=event %s', beautiful.config_path .. 'sounds/' .. sounds[s]), function() end)
 end
@@ -15,39 +20,39 @@ function M.notify()
 end
 
 function M.get_volume_state(cb)
-  awful.spawn.easy_async('pactl list sinks', function(stdout)
-    local mute = stdout:match('Mute:%s+(%a+)')
-    local volpercent = stdout:match('%s%sVolume:[%s%a-:%d/]+%s(%d+)%%')
+	awful.spawn.easy_async('pactl list sinks', function(stdout)
+		local mute = stdout:match('Mute:%s+(%a+)')
+		local volpercent = stdout:match('%s%sVolume:[%s%a-:%d/]+%s(%d+)%%')
 
-    if mute == 'yes' then
-      mute = true
-    else
-      mute = false
-    end
+		if mute == 'yes' then
+			mute = true
+		else
+			mute = false
+		end
 
-    volpercent = tonumber(volpercent)
+		volpercent = tonumber(volpercent)
 
-    cb(volpercent, mute)
-  end)
+		cb(volpercent, mute)
+	end)
 end
 
 function M.volumeUp()
-    awesome.spawn('pactl set-sink-volume @DEFAULT_SINK@ +'..step..'%')
-    -- awesome.emit_signal('evil::volume', next_vol, mute)
+	spawn('pactl set-sink-volume @DEFAULT_SINK@ +'..step..'%')
+	-- awesome.emit_signal('evil::volume', next_vol, mute)
 end
 
 function M.volumeDown()
-    awesome.spawn('pactl set-sink-volume @DEFAULT_SINK@ -'..step..'%')
-    -- awesome.emit_signal('evil::volume', next_vol, mute)
+	spawn('pactl set-sink-volume @DEFAULT_SINK@ -'..step..'%')
+	-- awesome.emit_signal('evil::volume', next_vol, mute)
 end
 
 function M.setVolume(vol)
-	awesome.spawn('pactl set-sink-volume @DEFAULT_SINK@ '..vol..'%')
+	spawn('pactl set-sink-volume @DEFAULT_SINK@ '..vol..'%')
 	-- awesome.emit_signal('evil::volume', next_vol, mute)
 end
 
 function M.muteVolume()
-    awesome.spawn('pactl set-sink-mute @DEFAULT_SINK@ toggle')
+	spawn('pactl set-sink-mute @DEFAULT_SINK@ toggle')
 end
 
 return M
