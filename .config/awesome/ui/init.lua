a19ec9737f8587f68ee732bca75af5ba886760bb
sammyette <torchedsammy@gmail.com>
2023-11-07 21:54:24 -0400

    awesome: load titlebars

--- .config/awesome/ui/init.lua
+++ .config/awesome/ui/init.lua
@@ -1,9 +1,7 @@
 local beautiful = require 'beautiful'
 
 require('ui.bars.' .. (beautiful.bar and beautiful.bar or 'default'))
-if beautiful.titlebar_type ~= 'none' then
-	require('ui.titlebars.' .. (beautiful.titlebar_type and beautiful.titlebar_type or 'default'))
-end
+require 'ui.titlebars'
 require 'ui.notifs'
 require 'ui.menu'
 
