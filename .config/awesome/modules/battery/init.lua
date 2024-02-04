d73e633b8a436f5dc6b444ec7c4994acca8ac355
sammyette <torchedsammy@gmail.com>
2024-02-04 16:02:01 -0400

    awesome(battery): dont animate on power save, return empty time if time is 0

--- .config/awesome/modules/battery/init.lua
+++ .config/awesome/modules/battery/init.lua
@@ -73,19 +73,28 @@ function M.time()
 	local state = M.status()
 
 	if state == 'Charging' then
+		if battery.time_to_full == 0 then return '' end
 		return formatTime(battery.time_to_full) .. ' until full'
 	else
+		if battery.time_to_empty == 0 then return '' end
 		return formatTime(battery.time_to_empty) .. ' until empty'
 	end
 end
 
 local manager = require 'modules.battery.managers.system76-power'
 function M.setProfile(profile)
+	if profile == 'powerSave' then
+		settings.noAnimate = true
+	else
+		settings.noAnimate = false
+	end
 	manager[profile .. 'Profile']()
 end
 
 function M.profile()
 	return manager.profile()
 end
+
+if M.profile() == 'powerSave' then settings.noAnimate = true end
 
 return M
