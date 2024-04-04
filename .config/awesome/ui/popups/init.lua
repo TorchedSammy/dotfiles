676eeb6807e2c33b1aee4eb8a31f45dd99783097
sammyette <torchedsammy@gmail.com>
2024-04-04 13:39:49 -0400

    fix: wrap popups require behind pcall

--- .config/awesome/ui/popups/init.lua
+++ .config/awesome/ui/popups/init.lua
@@ -1,6 +1,6 @@
 local beautiful = require 'beautiful'
 local settings = require 'conf.settings'
 
-require('ui.popups.' .. settings.theme)
---local ok, popups = pcall(require, )
+--require('ui.popups.' .. settings.theme)
+local ok, popups = pcall(require, 'ui.popups.' .. settings.theme)
 --if not ok then return end -- TODO: print error (check if the path doesnt exist ig)
