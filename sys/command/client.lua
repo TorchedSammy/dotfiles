40e58d323e759a579c3ac839b59ce81761624f7f
sammyette <torchedsammy@gmail.com>
2024-11-24 18:53:28 -0400

    feat: add more client commands

--- sys/command/client.lua
+++ sys/command/client.lua
@@ -3,6 +3,36 @@ local command = require 'sys.command'
 command.add {
 	name = 'client:focus',
 	action = function(c)
-		c:activate {}
+		c:activate { raise = true }
+	end
+}
+
+command.add {
+	name = 'client:move',
+	action = function(c)
+		c:activate { raise = true, action = 'mouse_move'  }
+	end
+}
+
+command.add {
+	name = 'client:resize',
+	action = function(c)
+		c:activate { raise = true, action = 'mouse_resize'  }
+	end
+}
+
+command.add {
+	name = 'client:maximize',
+	action = function(c)
+		c.maximized = not c.maximized
+		c:raise()
+	end
+}
+
+command.add {
+	name = 'client:fullscreen',
+	action = function(c)
+		c.fullscreen = not c.fullscreen
+		c:raise()
 	end
 }
